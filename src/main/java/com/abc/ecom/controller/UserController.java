0d88788c316ef58ef720b02f9a1a5c21dda8c9ea
vasudhara <vasudharash@gmail.com>
2025-11-21 16:22:40 +0530

    ecom-user-app - change return type to ResponseEntity

--- src/main/java/com/abc/ecom/controller/UserController.java
+++ src/main/java/com/abc/ecom/controller/UserController.java
@@ -3,6 +3,8 @@ package com.abc.ecom.controller;
 import com.abc.ecom.model.User;
 import com.abc.ecom.service.impl.UserServiceImpl;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,18 +19,25 @@ public class UserController {
     UserServiceImpl userServiceImpl;
 
     @GetMapping("/api/user")
-    public List<User> getListOfUsers() {
-        return userServiceImpl.featchAllUser();
+    public ResponseEntity<List<User>> getListOfUsers() {
+        return ResponseEntity.ok(userServiceImpl.featchAllUser());
+        // other way to use response entity
+        //return new ResponseEntity<>(userServiceImpl.featchAllUser(), HttpStatus.OK);
+
     }
 
     @GetMapping("/api/user/{id}")
-    public User getUser(@PathVariable Long id) {
-        return userServiceImpl.featchUser(id);
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        User user = userServiceImpl.featchUser(id);
+        if(user == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(userServiceImpl.featchUser(id));
     }
 
     @PostMapping("/api/user")
-    public String createUser(@RequestBody User user) {
+    public ResponseEntity<String> createUser(@RequestBody User user) {
         userServiceImpl.addUser(user);
-        return "User added successfully";
+        return ResponseEntity.ok("User added successfully");
     }
 }
